9b2e5577e3acd3b1db2ba6ea2d3f9cbaa3bd95b7
minhhoangbui <minhhoang0602kc@gmail.com>
2019-06-10 12:28:58 +0700

    test

--- loss.py
+++ loss.py
@@ -1,6 +1,7 @@
 import numbers
 import tensorflow as tf
 from tensorflow.python.ops import array_ops, math_ops
+from tensorflow.python.framework import dtypes
 
 def pairwise_distance(feature, squared=False):
     """Computes the pairwise distance matrix with numerical stability.
@@ -49,11 +50,11 @@ def get_at_indices(tensor, indices):
     return tf.gather_nd(tensor, tf.stack((counter, indices), -1))
 
 
-def batch_hard(dists, pids, margin, batch_precision_at_k=None):
+def triplet_hardest_loss(pdist_matrix, pids, margin, batch_precision_at_k=None):
     """Computes the batch-hard loss from arxiv.org/abs/1703.07737.
 
     Args:
-        dists (2D tensor): A square all-to-all distance matrix as given by cdist.
+        pdist_matrix (2D tensor): A square all-to-all distance matrix as given by cdist.
         pids (1D tensor): The identities of the entries in `batch`, shape (B,).
             This can be of any type that can be compared, thus also a string.
         margin: The value of the margin if a number, alternatively the string
@@ -70,9 +71,9 @@ def batch_hard(dists, pids, margin, batch_precision_at_k=None):
         positive_mask = tf.logical_xor(same_identity_mask,
                                        tf.eye(tf.shape(pids)[0], dtype=tf.bool))
 
-        furthest_positive = tf.reduce_max(dists*tf.cast(positive_mask, tf.float32), axis=1)
+        furthest_positive = tf.reduce_max(pdist_matrix*tf.cast(positive_mask, tf.float32), axis=1)
         closest_negative = tf.map_fn(lambda x: tf.reduce_min(tf.boolean_mask(x[0], x[1])),
-                                    (dists, negative_mask), tf.float32)
+                                    (pdist_matrix, negative_mask), tf.float32)
         # Another way of achieving the same, though more hacky:
         # closest_negative = tf.reduce_min(dists + 1e5*tf.cast(same_identity_mask, tf.float32), axis=1)
 
@@ -95,7 +96,7 @@ def batch_hard(dists, pids, margin, batch_precision_at_k=None):
     with tf.name_scope("monitoring"):
         # This is like argsort along the last axis. Add one to K as we'll
         # drop the diagonal.
-        _, indices = tf.nn.top_k(-dists, k=batch_precision_at_k+1)
+        _, indices = tf.nn.top_k(-pdist_matrix, k=batch_precision_at_k+1)
 
         # Drop the diagonal (distance to self is always least).
         indices = indices[:,1:]
@@ -122,12 +123,155 @@ def batch_hard(dists, pids, margin, batch_precision_at_k=None):
 
         # Finally, let's get some more info that can help in debugging while
         # we're at it!
-        negative_dists = tf.boolean_mask(dists, negative_mask)
-        positive_dists = tf.boolean_mask(dists, positive_mask)
+        negative_dists = tf.boolean_mask(pdist_matrix, negative_mask)
+        positive_dists = tf.boolean_mask(pdist_matrix, positive_mask)
 
         return diff, top1, prec_at_k, topk_is_same, negative_dists, positive_dists
 
 
+def masked_maximum(data, mask, dim=1):
+    """Computes the axis wise maximum over chosen elements.
+    Args:
+        data: 2-D float `Tensor` of size [n, m].
+        mask: 2-D Boolean `Tensor` of size [n, m].
+        dim: The dimension over which to compute the maximum.
+    Returns:
+        masked_maximums: N-D `Tensor`.
+        The maximized dimension is of size 1 after the operation.
+    """
+    axis_minimums = math_ops.reduce_min(data, dim, keepdims=True)
+    masked_maximums = math_ops.reduce_max(
+        math_ops.multiply(data - axis_minimums, mask), dim,
+        keepdims=True) + axis_minimums
+    return masked_maximums
+
+
+def masked_minimum(data, mask, dim=1):
+    """Computes the axis wise minimum over chosen elements.
+    Args:
+        data: 2-D float `Tensor` of size [n, m].
+        mask: 2-D Boolean `Tensor` of size [n, m].
+        dim: The dimension over which to compute the minimum.
+    Returns:
+        masked_minimums: N-D `Tensor`.
+        The minimized dimension is of size 1 after the operation.
+    """
+    axis_maximums = math_ops.reduce_max(data, dim, keepdims=True)
+    masked_minimums = math_ops.reduce_min(
+        math_ops.multiply(data - axis_maximums, mask), dim,
+        keepdims=True) + axis_maximums
+    return masked_minimums
+
+def triplet_semihard_loss(pdist_matrix, labels, margin=1.0):
+    """Computes the triplet loss with semi-hard negative mining.
+    The loss encourages the positive distances (between a pair of embeddings with
+    the same labels) to be smaller than the minimum negative distance among
+    which are at least greater than the positive distance plus the margin constant
+    (called semi-hard negative) in the mini-batch. If no such negative exists,
+    uses the largest negative distance instead.
+    See: https://arxiv.org/abs/1503.03832.
+    Args:
+        labels: 1-D tf.int32 `Tensor` with shape [batch_size] of
+        multiclass integer labels.
+        pdist_matrix: 2-D float `Tensor` of square all-to-all distance matrix. Embeddings should
+        be l2 normalized.
+        margin: Float, margin term in the loss definition.
+    Returns:
+        triplet_loss: tf.float32 scalar.
+    """
+    # Reshape [batch_size] label tensor to a [batch_size, 1] label tensor.
+    lshape = array_ops.shape(labels)
+    assert lshape.shape == 1
+    labels = array_ops.reshape(labels, [lshape[0], 1])
+
+    # Build pairwise binary adjacency matrix.
+    adjacency = math_ops.equal(labels, array_ops.transpose(labels))
+    # Invert so we can select negatives only.
+    adjacency_not = math_ops.logical_not(adjacency)
+
+    batch_size = array_ops.size(labels)
+
+    # Compute the mask.
+    pdist_matrix_tile = array_ops.tile(pdist_matrix, [batch_size, 1])
+    mask = math_ops.logical_and(
+        array_ops.tile(adjacency_not, [batch_size, 1]),
+        math_ops.greater(
+            pdist_matrix_tile, array_ops.reshape(
+                array_ops.transpose(pdist_matrix), [-1, 1])))
+    mask_final = array_ops.reshape(
+        math_ops.greater(
+            math_ops.reduce_sum(
+                math_ops.cast(mask, dtype=dtypes.float32), 1, keepdims=True),
+            0.0), [batch_size, batch_size])
+    mask_final = array_ops.transpose(mask_final)
+
+    adjacency_not = math_ops.cast(adjacency_not, dtype=dtypes.float32)
+    mask = math_ops.cast(mask, dtype=dtypes.float32)
+
+    # negatives_outside: smallest D_an where D_an > D_ap.
+    negatives_outside = array_ops.reshape(
+        masked_minimum(pdist_matrix_tile, mask), [batch_size, batch_size])
+    negatives_outside = array_ops.transpose(negatives_outside)
+
+    # negatives_inside: largest D_an.
+    negatives_inside = array_ops.tile(
+        masked_maximum(pdist_matrix, adjacency_not), [1, batch_size])
+    semi_hard_negatives = array_ops.where(
+        mask_final, negatives_outside, negatives_inside)
+
+    loss_mat = math_ops.add(margin, pdist_matrix - semi_hard_negatives)
+
+    mask_positives = math_ops.cast(
+        adjacency, dtype=dtypes.float32) - array_ops.diag(
+            array_ops.ones([batch_size]))
+
+    # In lifted-struct, the authors multiply 0.5 for upper triangular
+    #   in semihard, they take all positive pairs except the diagonal.
+    num_positives = math_ops.reduce_sum(mask_positives)
+
+    triplet_loss = math_ops.truediv(
+        math_ops.reduce_sum(
+            math_ops.maximum(
+                math_ops.multiply(loss_mat, mask_positives), 0.0)),
+        num_positives,
+        name='triplet_semihard_loss')
+
+    return triplet_loss, num_positives
+
+
+def batch_combined(embeddings, labels, margin, batch_precision_at_k=None):
+    '''
+    Computes the triplet loss with semi-hard negative mining and hardest negative mining.
+    The loss encourages the positive distances (between a pair of embeddings with
+    the same labels) to be smaller than the minimum negative distance among
+    which are at least greater than the positive distance plus the margin constant
+    (called semi-hard negative) in the mini-batch. If no such negative exists,
+    uses the largest negative distance instead.
+    See: https://arxiv.org/abs/1503.03832.
+    Args:
+        labels: 1-D tf.int32 `Tensor` with shape [batch_size] of
+        multiclass integer labels.
+        embeddings: 2-D float `Tensor` of embedding vectors. Embeddings should
+        be l2 normalized.
+        margin: Float, margin term in the loss definition.
+    Returns:
+        triplet_loss: tf.float32 scalar.
+    '''
+
+    pdist_mat = pairwise_distance(embeddings, squared=True)
+
+    hardest_losses = triplet_hardest_loss(pdist_matrix=pdist_mat, pids=labels, margin=margin)
+
+    loss_mean, num_loss = triplet_semihard_loss(pdist_mat, labels, margin=margin)
+
+    average_loss = (tf.reduce_sum(hardest_losses) + loss_mean * num_loss) / (tf.size(hardest_loss) + num_loss)
+
+    return average_loss
+
+
+
+
 LOSS_CHOICES = {
-    'batch_hard': batch_hard,
+    'batch_combined': batch_combined,
+    'batch_hard': triplet_hardest_loss,
 }
